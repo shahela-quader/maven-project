0ecd83888c0401164bbcaf5b67b3e3a58ae50b95
shahela <shahela@example.cpm>
2017-02-19 02:17:37 -0500

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,6 @@ public class Greeter {
 
   }
 
-  /** {@inheritDoc} */
-public int checkReturnTag(final int aTagIndex,
-                          JavadocTag[] aTags,
-                          int aLineNo)
-   */
 public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
